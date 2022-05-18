e3274cad96b496b64f3ffeee547cfd13def0ca07
wx9529 <xinwang213z@gmail.com>
2022-05-18 12:38:35 -0700

    Add update and delete cars

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,24 @@
 import Nav from '../components/Nav'
 import AuthModal from "../components/AuthModal"
-import {useState} from 'react'
-import {useCookies} from "react-cookie"
+import { useState } from 'react'
+import { useCookies } from "react-cookie"
 
 const Home = () => {
-    const [showModal, setShowModal] = useState(false)
-    const [isSignUp, setIsSignUp] = useState(true)
-    const [cookies, setCookie, removeCookie] = useCookies(['user'])
-    const authToken = cookies.AuthToken
+    const [showModal, setShowModal] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(true);
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
+    const authToken = cookies.AuthToken;
 
     const handleClick = () => {
         if (authToken) {
-            removeCookie('UserId', cookies.UserId)
-            removeCookie('AuthToken', cookies.AuthToken)
-            window.location.reload()
+            removeCookie('UserId', cookies.UserId);
+            removeCookie('AuthToken', cookies.AuthToken);
+            window.location.reload();
             return
         }
-        setShowModal(true)
-        setIsSignUp(true)
+        setShowModal(true);
+        setIsSignUp(true);
     }
-
     return (
         <div className="overlay">
             <Nav
@@ -30,14 +29,13 @@ const Home = () => {
                 setIsSignUp={setIsSignUp}
             />
             <div className="home">
-                <h1 className="primary-title">Swipe Right®</h1>
+                <h1 className="primary-title">AUTO DEAL®</h1>
                 <button className="primary-button" onClick={handleClick}>
                     {authToken ? 'Signout' : 'Create Account'}
                 </button>
 
-
                 {showModal && (
-                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
                 )}
             </div>
         </div>
--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const { MongoClient } = require('mongodb');
 const { v4: uuidv4 } = require('uuid');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const bcrypt = require('bcrypt')
-require('dotenv').config()
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 const url = 'mongodb+srv://xinwang:QxMKRWywKMtPyqrr@cluster0.krnu1.mongodb.net/?retryWrites=true&w=majority';
 
@@ -62,15 +62,15 @@ app.post('/login', async (req, res) => {
 
     const user = await users.findOne({ email });
 
-    const correctPassword = await bcrype.compare(password, user.hashed_password);
+    const correctPassword = await bcrypt.compare(password, user.hashed_password);
 
     if (user && correctPassword) {
       const token = jwt.sign(user, email, {
         expiresIn: 60 * 60
       })
-      res.status(201).json({ token, userId: user.user_id, email });
+      return res.status(201).json({ token, userId: user.user_id, email });
     }
-    res.status(400).send('Invalid Credentials');
+    return res.status(400).send('Invalid Credentials');
   } catch (err) {
     console.log(err);
   }
@@ -90,5 +90,103 @@ app.get('/users', async (req, res) => {
   }
 })
 
-app.listen(PORT, () => console.log('Server running on PORT ' + PORT));
 
+app.put('/user', async (req, res) => {
+  const client = new MongoClient(url);
+  const formData = req.body.formData;
+  try {
+    await client.connect();
+    const database = client.db('app-data');
+    const users = database.collection('users');
+
+    const query = { user_id: formData.user_id };
+    const updateDocument = {
+      $set: {
+        first_name: formData.first_name,
+        matches: formData.matches,
+        cars: [{
+          model: formData.Model,
+          year: formData.Year,
+          price: formData.Price,
+          url: formData.url
+        }]
+      },
+    }
+    const insertedUser = await users.updateOne(query, updateDocument);
+    res.send(insertedUser);
+  } finally {
+    await client.close();
+  }
+})
+
+//Get all cars in the database
+app.get('/cars', async (req, res) => {
+  const client = new MongoClient(url);
+  const userId = req.query.user_id;
+  try {
+    await client.connect();
+    const database = client.db('app-data');
+    const users = database.collection('users');
+    const query = { user_id: userId };
+    const foundUsers = await users.findOne(query);
+    console.log('foundUsers', foundUsers);
+    res.send(foundUsers)
+  } finally {
+    await client.close()
+  }
+})
+
+//insert a new car
+app.put('/cars', async (req, res) => {
+  const client = new MongoClient(url);
+  const userId = req.body.user_id;
+  const formData = req.body.formData;
+  const carId = req.body.id
+  console.log('userid', userId);
+  console.log('formData', formData);
+  console.log('carId', carId);
+  try {
+    await client.connect();
+    const database = client.db('app-data');
+    const users = database.collection('users');
+    const query = { user_id: userId };
+    const updateDocument = {
+      $push: {
+        cars: {
+          model: formData.model,
+          year: formData.year,
+          price: formData.price,
+          url: formData.url,
+          id: carId
+        }
+      },
+    }
+    const insertedCars = await users.updateOne(query, updateDocument);
+    res.send(insertedCars);
+  } finally {
+    await client.close();
+  }
+})
+//delete a car
+app.delete('/cars', async (req, res) => {
+  const client = new MongoClient(url);
+  console.log('req.query', req.query);
+  const userId = req.query.user_id;
+  const carId = req.query.id;
+  try {
+    await client.connect();
+    const database = client.db('app-data');
+    const users = database.collection('users');
+    const query = { user_id: userId };
+    const updateDocument = {
+      $pull: { "cars": { "id": carId } }
+    }
+    const user = await users.updateOne(query, updateDocument);
+    console.log('user', user);
+    res.send(user);
+  } finally {
+    await client.close();
+  }
+})
+
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT));
